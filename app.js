ad80fe402db38de73f26cfd1187ad9cf55817b8c
Marco Robol <marco.robol@unitn.it>
2023-03-27 15:39:08 +0200

    modifica readme file locale
    nuova variabile rebase

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const array = [0, 1, false, 2, undefined, '', 3, null, 'main', 'myFeature'];
 
 // hotfix
 
+const rebase = 'rebase';
+
 const compact = array.filter( function (el, index, array) {
     if (el)
         return true;
